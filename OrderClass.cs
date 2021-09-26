13cbb43c139512022acea94ad8dedb0c58854f38
mechrodancer <stickman331@gmail.com>
2021-09-25 18:25:23 -0700

    Update OrderClass.cs

--- OrderClass.cs
+++ OrderClass.cs
@@ -8,5 +8,49 @@ namespace Project2
 {
     class OrderClass
     {
+          private double unitPrice; //ticket unit price
+  private int cardNo;//credit card numbrt
+  private int amount; //ticket amount
+  private string senderId; // sender id
+  private string receverId;// recever if
+
+  public int CardNum
+        {
+            get {
+                return cardNo; 
+                }
+            set 
+            { 
+                cardNo = value; 
+            }
+        }
+  public int Amount
+        {
+            get {
+                return amount; 
+            }
+            set {
+                amount = value;
+            } 
+         }
+
+     public void setCardNo(int num)
+     {
+       cardNo = num;
+     }
+     public int getCardNo()
+     {
+       return cardNo;
+     }
+
+  
+     public void setAmount(int astuff)
+     {
+       amount = astuff;
+     }
+     public int getAmount()
+     {
+       return amount;
+     }
     }
 }
