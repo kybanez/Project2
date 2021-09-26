6f2245f6bb53c21ef0acc3406ac40769236168c2
kybanez <72380279+kybanez@users.noreply.github.com>
2021-09-25 20:57:18 -0700

    Add files via upload

--- MultiCellBuffer.cs
+++ MultiCellBuffer.cs
@@ -2,11 +2,64 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Project2
 {
     class MultiCellBuffer
     {
+        public static Semaphore semaphore = new Semaphore(0, 3);
+        private static OrderClass[] buffer = new OrderClass[3];
+       
+
+        public void setBuffer(OrderClass new_Order)
+        {
+           lock(buffer)
+            {
+                for (int i =0; i < buffer.Length; i++)
+                {
+                    if(buffer[i] == null)
+                    {
+                        buffer[i] = new_Order;
+
+                        return;
+                    }
+                }
+                semaphore.WaitOne();
+            }
+        }
+
+        public OrderClass getBuffer(string sendId)
+        {
+            lock(buffer)
+            {
+                OrderClass order = new OrderClass();
+
+                int index = -1;
+
+                for(int i = 0; i < buffer.Length; i++)
+                {
+                    if(buffer[i] != null)
+                    {
+                        order = buffer[i];
+                        
+                        if(order.getSenderId() == sendId)
+                        {
+                            index = i;
+                        }
+                    }
+                }
+
+                if(index != -1)
+                {
+                    buffer[index] = null;
+                }
+                semaphore.Release();
+                return order;
+            }
+        }
     }
+
+
 }
--- OrderClass.cs
+++ OrderClass.cs
@@ -8,49 +8,113 @@ namespace Project2
 {
     class OrderClass
     {
-          private double unitPrice; //ticket unit price
-  private int cardNo;//credit card numbrt
-  private int amount; //ticket amount
-  private string senderId; // sender id
-  private string receverId;// recever if
+        private double unitPrice; //ticket unit price; tickets received from theater
+        private int cardNo;//credit card numbrt
+        private int amount; //ticket amount
+        private int quantity; // # of tickets ordered
+        private string senderId; // sender id , thread ID, ticketbroker
+        private string receverId;// recever thread name , 
+        private DateTime timestamp = DateTime.Today;
 
-  public int CardNum
+
+        public int CardNum
         {
-            get {
-                return cardNo; 
-                }
-            set 
-            { 
-                cardNo = value; 
+            get
+            {
+                return cardNo;
+            }
+            set
+            {
+                cardNo = value;
             }
         }
-  public int Amount
+        public int Amount
         {
-            get {
-                return amount; 
+            get
+            {
+                return amount;
             }
-            set {
+            set
+            {
                 amount = value;
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
             } 
-         }
+            set
+            {
+                quantity = value;
+            }
+        }
 
-     public void setCardNo(int num)
-     {
-       cardNo = num;
-     }
-     public int getCardNo()
-     {
-       return cardNo;
-     }
+        public void setCardNo(int num)
+        {
+            cardNo = num;
+        }
+        public int getCardNo()
+        {
+            return cardNo;
+        }
 
-  
-     public void setAmount(int astuff)
-     {
-       amount = astuff;
-     }
-     public int getAmount()
-     {
-       return amount;
-     }
+
+        public void setAmount(int astuff)
+        {
+            amount = astuff;
+        }
+        public int getAmount()
+        {
+            return amount;
+        }
+
+        public DateTime TimeStamp
+        {
+            get
+            {
+                return timestamp;
+            } 
+            set
+            {
+                timestamp = value;
+            }
+        }
+
+        public string getSenderId()
+        {   
+            return senderId;
+        } 
+        
+        public void setSenderID(string s_ID)
+        {
+            senderId = s_ID;
+        }
+
+
+        public string getReceiverId()
+        {
+            return receverId;
+        }
+
+        public void setReceiverId(string r_ID)
+        {
+            receverId = r_ID;
+        }
+       
+
+        public override string ToString()
+        {
+            string order = "Order Ticket \n\t {SENDER ID: " + senderId
+                + "} \n\t {RECEIVER ID: " + receverId
+                + "} \n\t {CARD_NO: " + cardNo
+                + "} \n\t {QUANTITY: " + quantity
+                + "} \n\t {DATE CREATED: " + TimeStamp.ToString("D") + "}";
+
+            return order;
+        }
     }
+
 }
