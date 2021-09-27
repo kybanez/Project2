8ab920eea49a6fb0e427d002acfba56de1633d59
kybanez <72380279+kybanez@users.noreply.github.com>
2021-09-26 22:20:22 -0700

    Add files via upload

--- PricingModel.cs
+++ PricingModel.cs
@@ -30,14 +30,17 @@ namespace Project2
             Console.WriteLine("The number of tickets ordered {0} on  {1}.", num_orders, date_ordered);
             Console.WriteLine("Original Price was {0}", original_price);
 
+            double total_ticketPrice = original_price * num_orders;
+
+            Console.WriteLine("Price of the total number of ticket orders : ${0} ", total_ticketPrice);
             //discounts 
             double discountPerc = numOrderDiscount(num_orders) + availOrderDiscount(date_ordered);
-            double percent = discountPerc * 1;
+            double percent = discountPerc * 100;
 
-            double discPrice = original_price * discountPerc;
+            double discPrice = total_ticketPrice * discountPerc;
 
             //new calculated price
-            new_price = original_price - discPrice;
+            new_price = total_ticketPrice - discPrice;
 
             Console.WriteLine("The discount taken is {0} after a {1}%.", discPrice, percent);
             Console.WriteLine("The new price is {0}", new_price);
@@ -49,29 +52,30 @@ namespace Project2
         //Calculate timespan of date order and give discount
         private static double availOrderDiscount(DateTime date_ordered)
         {
-            TimeSpan dateDiff = DateTime.Now - date_ordered;
+            TimeSpan dateDiff = date_ordered - DateTime.Now;
             int num_days = Int32.Parse(dateDiff.Days.ToString());
+            Console.WriteLine("The number of days from order time {0}", num_days);
 
             if(num_days <= 5)
             {
-                return 0.10;
+                return 0.05;
             } else if (num_days >= 6 && num_days <= 10){
-                return 0.15;
+                return 0.10;
             } else if (num_days >= 11 && num_days <= 20){
-                return 0.20;
+                return 0.15;
             } else {
-                return 0.30;
+                return 0.20;
             }
         }
 
         //calculate discount based on number of orders
         private static double numOrderDiscount(int num_orders)
         {
-           if(num_orders == 1)
+           if(num_orders <= 5)
            {
-                return 1.10;
+                return 0.05;
            } else {
-                return 0.90;
+                return 0.10;
            }
         }
     }
